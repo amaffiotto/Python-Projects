584097a1860a468d5c25b41bbb76f76f0daf2857
Andrea <a.maffiotto.4046@vallauri.edu>
2026-01-15 23:51:26 +0100

    First Commit to load old stuff

---
+++ Python Problems/Topics/Taking user's input/Calculate birth year from current age/main.py
@@ -0,0 +1,10 @@
+# Take the user's current age as input
+current_age = int(input())
+
+# Current year
+current_year = 2023
+
+# Calculate the birth year
+year_of_birth = current_year - current_age
+# Print the birth year
+print(year_of_birth)
